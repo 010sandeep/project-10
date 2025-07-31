dbb498357dcd474b09d211f55586a4a5671e8528
sandeep gurjar <sandeepgurjardogne202@gmail.com>
2025-07-31 12:38:40 +0530

    25-07-2025

---
+++ Project_10/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * Sandeep Gurjar 
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
---
+++ Project_10/src/main/java/com/rays/ctl/EmployeeCtl.java
@@ -0,0 +1,33 @@
+package com.rays.ctl;
+
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rays.common.BaseCtl;
+import com.rays.common.ORSResponse;
+import com.rays.dto.EmployeeDTO;
+import com.rays.form.EmployeeForm;
+import com.rays.service.EmployeeServiceInt;
+
+
+@RestController
+@RequestMapping(value = "Employee")
+public class EmployeeCtl extends BaseCtl<EmployeeForm, EmployeeDTO, EmployeeServiceInt>{
+
+	 @GetMapping("/preload")
+		public ORSResponse preload() {
+			ORSResponse res = new ORSResponse(true);
+			HashMap<Integer, String> map=new HashMap<Integer, String>();
+			map.put(1, "indore");
+			map.put(2, "pune");
+			map.put(3, "mumbai");
+			
+			res.addResult("address", map);
+			return res;
+		}
+	
+
+}
